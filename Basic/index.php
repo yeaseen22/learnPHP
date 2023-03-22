77c0f197f1a754136d6196d20f16d68cb9441955
devilarmaan! <devilarmaan63@gmail.com>
2023-03-22 14:56:45 +0600

    add

--- Basic/index.php
+++ Basic/index.php
@@ -15,7 +15,7 @@ console.log(a['name']);
 </script> -->
 
 
-<?php
+
 
 // $a = 20;
 // $b = 10;
@@ -31,10 +31,37 @@ console.log(a['name']);
 
 
 
+<!Doctype html>
+<html>
 
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Document</title>
+</head>
+<body>
+<?php
 
-
+if(isset($_POST['btn'])){
+    $a = $_POST['name'];
+    $b = $_POST['email'];
+    echo $a;
+}
 
 ?>
+
+<form method="POST">
+<input type="text" name="name">
+<input type="text" name="email">
+<button>Ok</button>
+
+</form>
+
+</body>
+</html>
+
+
+
+
 
 
